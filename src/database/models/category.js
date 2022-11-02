0b2073ff5108223ba59f7526ae51997a64952ff2
Luca <lucagelmini@github.com>
2022-11-02 12:47:38 -0300

    add: transaction model

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      Category.hasMany(models.Transaction, { foreignKey: 'categoryId' });
     }
   };
   Category.init({
---
+++ src/database/models/transaction.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Transaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Transaction.belongsTo(models.Category, { foreignKey: 'categoryId' });
+      Transaction.belongsTo(models.User, { foreignKey: 'originUserId' });
+      Transaction.belongsTo(models.User, { foreignKey: 'destinationUserId' });
+    }
+  }
+  Transaction.init({
+    concept: DataTypes.STRING,
+    amount: DataTypes.DECIMAL(10, 2),
+    type: DataTypes.STRING,
+    transactionDate: DataTypes.STRING,
+    categoryId: DataTypes.INTEGER,
+    originUserId: DataTypes.INTEGER,
+    destinationUserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Transaction',
+  });
+  return Transaction;
+};
