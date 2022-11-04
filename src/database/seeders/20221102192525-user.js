0455e6ed2e221409ab2ab6b88afb117964879d95
Federico Gonzalez <alambratore@hotmail.com>
2022-11-04 07:49:20 -0300

    feat: New User model - column BALANCE added

--- src/database/seeders/20221102192525-user.js
+++ src/database/seeders/20221102192525-user.js
@@ -1,35 +1,53 @@
 'use strict';
 const { faker } = require('@faker-js/faker');
 
-
-const users = Array.from({ length: 50 }).map(() => {
+const users = Array.from({ length: 22 }).map(() => {
   return {
     firstName: faker.name.firstName(),
     lastName: faker.name.lastName(),
-    email: faker.internet.email(),
+    email: faker.internet.email().toLocaleLowerCase(),
     address: faker.address.streetAddress(),
-    password: "$2a$10$d/lnn7UZrdJ2ltfM5QMZkumOrghswpLGerfXLmIcko8cbxU7vHGYy",
+    password: '$2a$10$d/lnn7UZrdJ2ltfM5QMZkumOrghswpLGerfXLmIcko8cbxU7vHGYy',
     avatar: faker.system.commonFileName('jpg'),
-    roleId: 3
-
-  }
-})
+    roleId: 3,
+    status: 'active',
+    balance: 0
+  };
+});
 
 const externalAgentUser = {
+  firstName: 'Dios',
+  lastName: 'García',
+  email: 'ext@usr.com',
+  password: '$2a$10$d/lnn7UZrdJ2ltfM5QMZkumOrghswpLGerfXLmIcko8cbxU7vHGYy',
+  roleId: 1,
+  status: 'active',
+  balance: 10000000.5
+};
 
-  firstName: "extusr",
-  lastName: "extusr",
-  email: "ext@usr.com",
-  password: "$2a$10$d/lnn7UZrdJ2ltfM5QMZkumOrghswpLGerfXLmIcko8cbxU7vHGYy",
-  roleId: 1
+const userOne = {
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'uno@mail.com',
+  password: '$2a$10$d/lnn7UZrdJ2ltfM5QMZkumOrghswpLGerfXLmIcko8cbxU7vHGYy',
+  roleId: 3,
+  status: 'active',
+  balance: 0
+};
 
-
-}
-
+const userTwo = {
+  firstName: 'jane',
+  lastName: 'Doe',
+  email: 'dos@mail.com',
+  password: '$2a$10$d/lnn7UZrdJ2ltfM5QMZkumOrghswpLGerfXLmIcko8cbxU7vHGYy',
+  roleId: 3,
+  status: 'active',
+  balance: 0
+};
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Users', [externalAgentUser, ...users], {});
+    await queryInterface.bulkInsert('Users', [externalAgentUser, userOne, userTwo, ...users], {});
   },
 
   async down(queryInterface, Sequelize) {
