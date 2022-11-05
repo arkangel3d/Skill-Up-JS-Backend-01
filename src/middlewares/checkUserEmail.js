d5198ef21ed56d8cf5b00ae37cc57d1326472419
Federico Gonzalez <alambratore@hotmail.com>
2022-11-04 21:05:52 -0300

    fix: New responses, Its depends on role_id

--- src/middlewares/checkUserEmail.js
+++ src/middlewares/checkUserEmail.js
@@ -1,10 +1,35 @@
+const { Op } = require('sequelize');
+
 const { User } = require('../database/models');
+const { ID_ROLE_EXTAGENCY, ID_ROLE_ADMIN } = require('../constanst/roles');
 
 const checkUserEmail = async (req, res, next) => {
   const { email } = req.params;
   const { roleId } = req.user;
 
-  const user = await User.findOne({ where: { email } });
+  if (roleId === ID_ROLE_EXTAGENCY || roleId === ID_ROLE_ADMIN) {
+    const user = await User.findOne({
+      where: {
+        email
+      },
+      attributes: ['id', 'firstName', 'lastName', 'email', 'password', 'address', 'avatar', 'balance', 'status', 'balance']
+    });
+
+    req.user = user;
+
+    return next();
+  }
+
+  const user = await User.findOne({
+    where: {
+      email,
+      roleId: {
+        [Op.gte]: roleId
+      },
+      status: 'active'
+    },
+    attributes: ['id', 'firstName', 'lastName', 'roleId', 'status']
+  });
 
   if (!user || user?.roleId < roleId) {
     return res.status(404).json({
@@ -14,7 +39,12 @@ const checkUserEmail = async (req, res, next) => {
     });
   }
 
-  req.user = user;
+  req.user = {
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    satus: user.status
+  };
 
   next();
 };
--- src/middlewares/checkUserId.js
+++ src/middlewares/checkUserId.js
@@ -1,10 +1,32 @@
+const { Op } = require('sequelize');
+
 const { User } = require('../database/models');
+const { ID_ROLE_EXTAGENCY, ID_ROLE_ADMIN } = require('../constanst/roles');
 
 const checkUserId = async (req, res, next) => {
   const { id } = req.params;
   const { roleId } = req.user;
 
-  const user = await User.findByPk(id);
+  if (roleId === ID_ROLE_EXTAGENCY || roleId === ID_ROLE_ADMIN) {
+    const user = await User.findByPk(id, {
+      attributes: ['id', 'firstName', 'lastName', 'email', 'password', 'address', 'avatar', 'balance', 'status', 'balance']
+    });
+
+    req.user = user;
+
+    return next();
+  }
+
+  const user = await User.findOne({
+    where: {
+      id,
+      roleId: {
+        [Op.gte]: roleId
+      },
+      status: 'active'
+    },
+    attributes: ['id', 'firstName', 'lastName', 'roleId']
+  });
 
   if (!user || user?.roleId < roleId) {
     return res.status(404).json({
@@ -14,7 +36,11 @@ const checkUserId = async (req, res, next) => {
     });
   }
 
-  req.user = user;
+  req.user = {
+    id,
+    firstName: user.firstName,
+    lastName: user.lastName
+  };
 
   next();
 };
