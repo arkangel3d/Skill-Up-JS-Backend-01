ece6adb7c6b9aaff5af19e96f9d45c21df08ff6c
Federico Gonzalez <alambratore@hotmail.com>
2022-11-09 15:43:28 -0300

    New login response and minor fixes

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -170,10 +170,8 @@ module.exports = {
         res,
         message: 'Usuario editado.',
         body: {
-          id,
           firstName,
-          lastName,
-          password: hashedPassword
+          lastName
         }
       });
     } catch (error) {
